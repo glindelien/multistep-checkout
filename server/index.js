cb312c904cc8eaa3e21e8ecd36521192a64069e7
Grace Lindelien <gracelindelien@gmail.com>
2021-01-08 20:05:56 -0800

    Create Hello World Express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
